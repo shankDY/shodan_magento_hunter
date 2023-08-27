a835fd6f4ec2ceb32afddd7b1bc3c168a3584310
ShankDY <ShankDY@yandex.ru>
2023-08-27 20:21:48 +0300

    add shodan_magento_hunter.py and README.md

---
+++ shodan_magento_hunter.py
@@ -0,0 +1,40 @@
+import shodan
+import concurrent.futures
+
+def get_magento_addr(result):
+    ip = result['ip_str']
+    ports = result.get('ports', [])
+    if ports:
+        ports_str = ', '.join(map(str, ports))
+        return f"{ip}:{ports_str}"
+    else:
+        return ip
+
+def search_magento_sites(api, max_addresses):
+    try:
+        query = 'X-Magento-Vary'
+        results = api.search(query, limit=max_addresses)
+        file_path = 'magento_ip.txt'
+        
+        ip_list = []
+        
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(get_magento_addr, result) for result in results['matches']]
+            
+            for future in concurrent.futures.as_completed(futures):
+                ip_list.append(future.result())
+        
+        with open(file_path, 'w') as file:
+            file.write('\nhttp://'.join(ip_list))
+        
+        print(f"Адреса собраны и файл {file_path} успешно создан")
+        
+    except shodan.APIError as e:
+        print(f"Ошибка Shodan API: {e}")
+
+if __name__ == '__main__':
+    max_addresses = int(input("Введите максимальное количество адресов (не более 500): "))
+    max_addresses = min(max_addresses, 500)
+    SHODAN_API_KEY = 'YOUR_API_KEY'
+    api = shodan.Shodan(SHODAN_API_KEY)
+    search_magento_sites(api, max_addresses)
